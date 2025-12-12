b67b785a75ff7818ce237b1a3d033db187735c7a
Shivam <shivamarora.07.sa@gmail.com>
2025-12-12 10:29:30 +0530

    "updated api when adding new macros"

---
+++ app/api/macros/new/route.js
@@ -0,0 +1,52 @@
+//when write a post request
+//db connect -> 
+
+import { connectDB } from "../../../../lib/connectDB";
+import Macros from '../../../../models/macros'
+
+export async function POST(request){
+    await connectDB();
+    const body = await request.json();
+    const dateString = new Date(body.consumedAt).toISOString().slice(0, 10);
+    const existing = await Macros.findOne({ date: dateString });
+
+    const foodItem = {
+    name: body.name,
+    calories: body.calories,
+    protein: body.protein,
+    carbs: body.carbs,
+    fat: body.fat,
+    sugar: body.sugar || 0,
+    caffeine: body.caffeine || 0,
+    consumedAt: new Date(body.consumedAt),
+  };
+
+    if (existing) {
+    // Add food to the food array
+    existing.foods.push(foodItem);
+    // Update totals
+    existing.calories += body.calories;
+    existing.protein += body.protein;
+    existing.carbs += body.carbs;
+    existing.fat += body.fat;
+    existing.sugar += body.sugar || 0;
+    existing.caffeine += body.caffeine || 0;
+
+    await existing.save();
+    return Response.json(existing);
+  }
+    const newMacro = await Macros.create({
+    date: dateString, 
+    name: body.name,
+    calories: body.calories,
+    sugar: body.sugar,
+    fat: body.fat,
+    protein: body.protein,
+    carbs: body.carbs,
+    caffeine: body.caffeine,
+    quantity: body.quantity,
+    consumedAt: body.consumedAt,
+    foods: [foodItem],
+  });
+  return Response.json(newMacro);
+}
