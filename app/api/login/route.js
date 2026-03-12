f1c5edef95c94bcc805138b6a6e86380a64d6267
Shivam <shivamarora.07.sa@gmail.com>
2026-03-12 09:20:01 +0530

    Used json web token for session storage

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -3,6 +3,9 @@ import { withErrorWrapper } from "../../../lib/withErrorWrapper"
 import { APIError } from "../../../lib/APIError";
 import Register from "./../../../models/register";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { cookies } from "next/headers";
+const JWT_SECRET = process.env.JWT_SECRET
 
 export const POST = withErrorWrapper(async(request) => {
     await connectDB();
@@ -23,5 +26,23 @@ export const POST = withErrorWrapper(async(request) => {
     if(!passwordMatch){
         throw new APIError("Wrong Password", 400);
     }
+    //generate a JWT Token
+    const token = jwt.sign(
+        //payload data of user, not encrypted, just encoded
+        {userId: exisitingUser._id, email: exisitingUser.email},
+        //
+        JWT_SECRET,
+        //can accept d, m, s-> day, month, seconds
+        {expiresIn: "1h"}
+    );
+    //Store the token in a cookie (session)
+    //in newer Next.js (15+/16). cookies() is now async
+    const cookieStore = await cookies();
+    cookieStore.set("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 60 * 60
+    });
     return Response.json({status: "ok", message: "You have logged in"})
 })
